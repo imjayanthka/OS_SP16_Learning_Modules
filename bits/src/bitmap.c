fde879b0d11d56dda718c480676f9eb97db4ed6f
Jayanth Kanugo <jkdt2@mail.missouri.edu>
2016-01-29 14:26:49 -0600

    Strings [1-4]: Bits intial try

--- bits/src/bitmap.c
+++ bits/src/bitmap.c
@@ -9,20 +9,26 @@ struct bitmap {
 bitmap_t *bitmap_create(size_t n_bits) {
 	if(0 < n_bits && n_bits <= 255 ) {
 		struct bitmap bitmapCreated;
-		size_t bit_count_temp = n_bits;
-		size_t byte_count_temp = n_bits/8;
-		bitmapCreated.data = (uint8_t *)malloc(byte_count_temp);
-		bitmapCreated.bit_count = bit_count_temp;
-		bitmapCreated.byte_count = byte_count_temp;
-		struct bitmap *ptr = &bitmapCreated;
+		bitmapCreated.bit_count = n_bits;
+		bitmapCreated.byte_count = n_bits/8;
+		if(n_bits%8 != 0)
+			bitmapCreated.data = (uint8_t *)malloc((int)n_bits/8 + 1);
+		else
+			bitmapCreated.data = (uint8_t *)malloc((int)n_bits/8);
+		struct bitmap * ptr = &bitmapCreated;
+		for(int i = 0; i < n_bits; i++)
+			bitmap_set(ptr, i);
 		return ptr;
 	}
 	return NULL;
 }
 
 bool bitmap_set(bitmap_t *const bitmap, const size_t bit) {
-
-	return false;
+	
+	size_t flag = 1;
+	flag = flag << (bit%8);
+	bitmap->data =&(bitmap->data | flag);
+	return true;
 }
 
 bool bitmap_reset(bitmap_t *const bitmap, const size_t bit) {
--- strings/src/sstring.c
+++ strings/src/sstring.c
@@ -7,23 +7,61 @@
 
 bool string_valid(const char *str, const size_t length) {
 
+	if(str) {
+		if(length != 0) {
+			if(*(str+length-1) == '\0')
+				return true;
+		}
+	}
 	return false;
 }
 
 char *string_duplicate(const char *str, const size_t length) {
-
+	if(str) {
+		if(length != 0) {
+			int sizeLeng = strlen(str);
+			if(sizeLeng+1 >= length) {
+				char* newString = (char *)malloc(length);
+				if(newString) {
+					for(int i = 0; i < length; i++) {
+						*(newString+i) = *(str+i);
+					}
+					*(newString+length) = '\0';
+					return newString;
+				}
+			}
+		}
+	}
 	return NULL;
 
 }
 
 bool string_equal(const char *str_a, const char *str_b, const size_t length) {	
 	
+	if(str_a && str_b) {
+		if(length != 0) {
+			if(strlen(str_a) != 0 || strlen(str_b) != 0) {
+				for (int i = 0; i < length; i++) {
+					if(*(str_a+i) != *(str_b+i))
+						return false;
+				}
+				return true;
+			}
+			else
+				return true;
+		}
+	}
 	return false;
 }
 
 int string_length(const char *str, const size_t length) {
-
-	return 0; 
+	if(str || length == 0) {
+		for (int i = 0; i < length; i++) {
+			if(str[i] == '\0')
+				return i;	
+		}	
+	}
+	return -1;
 }
 
 int string_tokenize(const char *str, const char *delims, const size_t str_length,char **tokens, const size_t max_token_length, const size_t requested_tokens) {
